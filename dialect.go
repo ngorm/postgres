6118355240217ffc9937f04925c3bb31f5bc9f45
gernest <geofreyernest@live.com>
2017-02-03 14:20:43 +0300

    Revert "Import the driver with the dialect"
    This reverts commit 49099d9601f22466bb932a2cc457b4cfe70e341e.

--- dialect.go
+++ dialect.go
@@ -1,15 +1,11 @@
 package gorm
 
 import (
-	"database/sql"
-	"database/sql/driver"
 	"fmt"
 	"reflect"
 	"strings"
 	"time"
 
-	_ "github.com/lib/pq"
-	"github.com/lib/pq/hstore"
 	"github.com/ngorm/common"
 	"github.com/ngorm/ngorm/model"
 )
@@ -160,49 +156,4 @@ func isUUID(value reflect.Value) bool {
 	typename := value.Type().Name()
 	lower := strings.ToLower(typename)
 	return "uuid" == lower || "guid" == lower
-}
-
-type Hstore map[string]*string
-
-// Value get value of Hstore
-func (h Hstore) Value() (driver.Value, error) {
-	hstore := hstore.Hstore{Map: map[string]sql.NullString{}}
-	if len(h) == 0 {
-		return nil, nil
-	}
-
-	for key, value := range h {
-		var s sql.NullString
-		if value != nil {
-			s.String = *value
-			s.Valid = true
-		}
-		hstore.Map[key] = s
-	}
-	return hstore.Value()
-}
-
-// Scan scan value into Hstore
-func (h *Hstore) Scan(value interface{}) error {
-	hstore := hstore.Hstore{}
-
-	if err := hstore.Scan(value); err != nil {
-		return err
-	}
-
-	if len(hstore.Map) == 0 {
-		return nil
-	}
-
-	*h = Hstore{}
-	for k := range hstore.Map {
-		if hstore.Map[k].Valid {
-			s := hstore.Map[k].String
-			(*h)[k] = &s
-		} else {
-			(*h)[k] = nil
-		}
-	}
-
-	return nil
 }
